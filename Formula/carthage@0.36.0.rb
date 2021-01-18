519660d407b51a5a38ea6e945f6ecf88657b961b
Alexey Ushakov <alexey.ushakov@deliveryhero.com>
2021-01-18 15:36:36 +0100

    Add outdated carthage version

---
+++ Formula/carthage@0.36.0.rb
@@ -0,0 +1,24 @@
+class CarthageAT0360 < Formula
+  desc "Decentralized dependency manager for Cocoa"
+  homepage "https://github.com/Carthage/Carthage"
+  url "https://github.com/Carthage/Carthage.git",
+      tag:      "0.36.0",
+      revision: "499c9cf8dafe5979e761397721f319db8bbe5859",
+      shallow:  false
+  license "MIT"
+  head "https://github.com/Carthage/Carthage.git", shallow: false
+
+  depends_on xcode: ["10.0", :build]
+
+  def install
+    system "make", "prefix_install", "PREFIX=#{prefix}"
+    bash_completion.install "Source/Scripts/carthage-bash-completion" => "carthage"
+    zsh_completion.install "Source/Scripts/carthage-zsh-completion" => "_carthage"
+    fish_completion.install "Source/Scripts/carthage-fish-completion" => "carthage.fish"
+  end
+
+  test do
+    (testpath/"Cartfile").write 'github "jspahrsummers/xcconfigs"'
+    system bin/"carthage", "update"
+  end
+end
